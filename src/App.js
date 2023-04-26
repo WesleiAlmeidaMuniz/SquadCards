a439c9645ed60d279972f59c91cb572d41fe1c06
Weslei Almeida Muniz <weslei_almeida@yahoo.com.br>
2023-04-26 16:51:24 -0300

    Remove e Delete.
    Criado função para remover os cards, tanto na tela como no localStorage.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
 import Rodape from './componentes/Rodape';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
@@ -45,54 +46,57 @@ function App() {
         },
     ]
 
-    const timeantigo = [
+    let timeantigo = [
         {
-        cargo: "Dozeiro",
-        imagem: "https://github.com/wesleialmeidamuniz.png",
-        nome: "Shadowkin",
-        time: "Fortnite"
-    },
-    {
-        cargo: "Canhão de Vidro",
-        imagem: "https://i.ibb.co/3YnpFtK/Imagem-do-Whats-App-de-2023-04-15-s-19-15-36.jpg",
-        nome: "Horizon Lest",
-        time: "Fortnite"
-    },
-    {
-        cargo: "Atiradora de Elite",
-        imagem: "https://i.ibb.co/vxZYyBK/dani.png",
-        nome: "The Darkness",
-        time: "Fortnite"
-    },
-    {
-        cargo: "Rushador",
-        imagem: "https://i.ibb.co/6X4t778/kok.png",
-        nome: "Los Kok",
-        time: "Fortnite"
-    }
+            id: uuidv4(),    
+            cargo: "Dozeiro",
+            imagem: "https://github.com/wesleialmeidamuniz.png",
+            nome: "Shadowkin",
+            time: "Fortnite"
+        },
+        {
+            id: uuidv4(),
+            cargo: "Canhão de Vidro",
+            imagem: "https://i.ibb.co/3YnpFtK/Imagem-do-Whats-App-de-2023-04-15-s-19-15-36.jpg",
+            nome: "Horizon Lest",
+            time: "Fortnite"
+        },
+        {
+            id: uuidv4(),
+            cargo: "Atiradora de Elite",
+            imagem: "https://i.ibb.co/vxZYyBK/dani.png",
+            nome: "The Darkness",
+            time: "Fortnite"
+        },
+        {
+            id: uuidv4(),
+            cargo: "Rushador",
+            imagem: "https://i.ibb.co/6X4t778/kok.png",
+            nome: "Los Kok",
+            time: "Fortnite"
+        }
     ]
     
-    if(localStorage.hasOwnProperty('timesalvo')){
-        timeantigo = JSON.parse(localStorage.getItem('timesalvo'))
+    //Este Loop recupera os dados salvos no LocalStorage
+    for(var i = 0; i < localStorage.length; i++){
+        console.log(localStorage.getItem(localStorage.key(i)))
+        //Este .Push joga os dados do localStorage para a var Timeantigo, irá renderizar na pagina com os times padrão.
+        timeantigo.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
     }
-
+    
     const [colaboradores, setColaboradores] = useState(timeantigo)
 
     const aoNovoColaboradorAdicionado = (colaborador) => {
         setColaboradores([...colaboradores, colaborador])
-
-        if(localStorage.hasOwnProperty('timesalvo')){
-            timeantigo = JSON.parse(localStorage.getItem('timesalvo'))
-        }
-
-        timeantigo.push(colaborador)
-
-        localStorage.setItem('timesalvo', JSON.stringify(timeantigo))
-
+        //Este .setItem salva o dado do colaborador criado no localStorage
+        localStorage.setItem(colaborador.id, JSON.stringify(colaborador))
+        
     }
 
-    function deletarColaborador()   {
-        console.log('Deletando colaborador')
+    function deletarColaborador(id){
+       setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id))
+        //Este .removeItem remove os dados salvos no LocalStorage
+       localStorage.removeItem(id);
     }
 
     return (
--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,9 +1,14 @@
+import { AiFillCloseCircle } from 'react-icons/ai';
 import './Colaborador.css'
 
 const Colaborador = ({colaborador, corDeFundo, aoDeletar}) => {
     return(
         <div className='colaborador'>
-            <div className='deletar' onClick={aoDeletar}>deletar</div>
+            <AiFillCloseCircle 
+                size={25} 
+                className='deletar' 
+                onClick={() => aoDeletar(colaborador.id)}
+            />
             <div className='cabecalho' style={{backgroundColor:corDeFundo}}>
                 <img src={colaborador.imagem} alt={colaborador.nome}/>
             </div>
